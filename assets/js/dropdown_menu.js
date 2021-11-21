6e5c91697339505ac32e49b3939a23f33b70a647
ZolsKe <zoltan.the.kepes@gmail.com>
2021-11-21 21:27:47 +0000

    fix: nav-bar float, responsive, hide hamburger

--- assets/js/dropdown_menu.js
+++ assets/js/dropdown_menu.js
@@ -6,15 +6,12 @@
  //  opens the details menu
  function detailsOpener_faq() {
      document.getElementById("faq").setAttribute("open", "");
-     document.getElementById("toggel_js").classList.toggle("show");
  }
 
  function detailsOpener_about_us() {
      document.getElementById("about_us").setAttribute("open", "");
-     document.getElementById("toggel_js").classList.toggle("show");
  }
 
  function detailsOpener_contact_us() {
      document.getElementById("contact_us").setAttribute("open", "");
-     document.getElementById("toggel_js").classList.toggle("show");
  }
